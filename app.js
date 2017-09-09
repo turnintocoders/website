46444dfd80ea616279c81229f8b047f8f49d94ea
Matteo Giaccone <matteo.giaccone@vitsoe.com>
2017-09-09 16:01:27 +0200

    Auto update Meetup date

--- app.js
+++ app.js
@@ -9,3 +9,30 @@ function toggleVisibility(selector) {
 		element.style.display = "block";
 	}
 }
+
+function meetupLinks() {
+	return document.querySelectorAll('[data-meetup-event]');
+}
+
+function updateMeetupDetails(jsonp) {
+	var meetupURL = jsonp.data[0].link;
+	var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	var date = new Date(jsonp.data[0].time)
+	var meetupDate = date.toLocaleDateString('it-IT', options);
+	var meetupPlaceholders = meetupLinks();
+	for (var i in meetupPlaceholders ) {
+		meetupPlaceholders[i].innerHTML = meetupDate;
+		meetupPlaceholders[i].href = meetupURL;
+	}
+}
+
+function getMeetupData() {
+	if (meetupLinks().length == 0) {
+		return;
+	}
+	var script = document.createElement('script');
+	script.src = 'http://api.meetup.com/turn-into-coders/events' + '?callback=updateMeetupDetails';
+	document.getElementsByTagName('head')[0].appendChild(script);
+}
+getMeetupData();
+
